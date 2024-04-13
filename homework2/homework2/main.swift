c4064f74cffe1648621a3ac4cc7fc07602977933
kabulov <n.kabulov@cft.ru>
2024-04-13 19:50:54 +0500

    Сделал задачи по инкапсуляции

--- homework2/homework2/main.swift
+++ homework2/homework2/main.swift
@@ -149,3 +149,175 @@ let bankAcc = BankAccount(bin: "KZ1928324043923", balance: 1000)
 bankAcc.replenishment(amount: 100)
 bankAcc.withdraw(amount: 2000)
 bankAcc.withdraw(amount: 300)
+
+
+//MARK: - ENCAPSULATION
+
+///Создайте класс "Банковский счет" с приватным свойством "баланс". Реализуйте методы для пополнения и снятия денег со счета, которые обновляют значение баланса. Установите условие, что снятие денег возможно только при наличии достаточного баланса. Создайте объект банковского счета и проверьте работу методов.
+
+class BankAccountEncapsulation {
+	private var balance: Int
+
+	init(balance: Int) {
+		self.balance = balance
+	}
+
+	func withdraw(amount: Int) {
+		if amount > balance {
+			print("Not enough money. Current balance: \(balance)")
+		} else {
+			balance -= amount
+			print("\(amount) was withdrawed. Current balance: \(balance)")
+		}
+	}
+
+	func replenishment(amount: Int) {
+		balance += amount
+		print("\(amount) was replenishmented. Current balance: \(balance)")
+	}
+}
+
+let bankAccount = BankAccountEncapsulation(balance: 100)
+bankAccount.withdraw(amount: 50)
+bankAccount.replenishment(amount: 150)
+
+///Создайте класс "Студент" с приватными свойствами "имя", "возраст" и "средний балл". Реализуйте методы для установки значений свойств и получения значений свойств. Установите условие, что средний балл может быть установлен только в диапазоне от 0 до 5. Создайте объект студента и проверьте работу методов.
+
+class StudentEncapsulation {
+	private var name: String = "Hypc"
+	private var age: Int = 18
+	private var overall: Float = 4.5
+
+	func setStudentName(_ name: String) {
+		self.name = name
+	}
+
+	func setStudentAge(_ age: Int) {
+		self.age = age
+	}
+
+	func setStudentOverall(_ overall: Float) {
+		if overall > 0 && overall < 5 {
+			self.overall = overall
+		} else {
+			print("Overall can be only from 0 to 5")
+		}
+	}
+
+	func getStudentName() -> String {
+		self.name
+	}
+
+	func getStudentAge() -> Int {
+		self.age
+	}
+
+	func getStudentOverall() -> Float {
+		self.overall
+	}
+}
+
+let student = StudentEncapsulation()
+print(student.getStudentAge())
+print(student.getStudentName())
+print(student.getStudentOverall())
+student.setStudentName("An")
+student.setStudentAge(20)
+student.setStudentOverall(6)
+student.setStudentOverall(4)
+print(student.getStudentAge())
+print(student.getStudentName())
+print(student.getStudentOverall())
+
+///Создайте класс "Телефон" с приватным свойством "номер телефона". Реализуйте методы для установки и получения номера телефона. Установите условие, что номер телефона должен быть валидным и соответствовать определенному формату. Создайте объект телефона и проверьте работу методов.
+
+class Phone {
+	private var number: String = ""
+
+	func setNumber(_ number: String) {
+		if validateNumber(number) {
+			self.number = number
+		} else {
+			print("Phone number must be format like +X(XXX)XXX-XX-XX")
+		}
+	}
+
+	func getNumber() -> String {
+		self.number
+	}
+
+	private func validateNumber(_ number: String) -> Bool {
+		guard number.count >= 13 else { return false }
+		if number.hasPrefix("+") &&
+			number[number.index(number.startIndex, offsetBy: 2)] == "(" &&
+			number[number.index(number.startIndex, offsetBy: 6)] == ")" &&
+			number[number.index(number.startIndex, offsetBy: 10)] == "-" &&
+			number[number.index(number.startIndex, offsetBy: 13)] == "-"
+		{
+			return true
+		}
+		return false
+	}
+}
+
+let phoneNumber = Phone()
+print(phoneNumber.getNumber())
+phoneNumber.setNumber("+")
+phoneNumber.setNumber("+7(707)323-55-44")
+print(phoneNumber.getNumber())
+
+
+///Создайте класс "Книга" с приватным свойством "название". Реализуйте методы для установки и получения названия книги. Установите условие, что название книги должно быть длиннее определенного значения. Создайте объект книги и проверьте работу методов.
+
+class BookEncapsulation {
+	private var name: String = ""
+
+	func setName(_ name: String) {
+		if isValidNameBookName(name) {
+			self.name = name
+		} else {
+			print("not valid book name")
+		}
+	}
+
+	func getName() {
+		print("\(self.name)")
+	}
+
+	private func isValidNameBookName(_ name: String) -> Bool {
+		if name.count > 10 {
+			return true
+		}
+		return false
+	}
+}
+var bookEnc = BookEncapsulation()
+bookEnc.setName("xzzxcsssxzc")
+bookEnc.getName()
+
+///Создайте класс "Автомобиль" с приватными свойствами "марка" и "модель". Реализуйте методы для установки и получения значений свойств. Установите условие, что модель автомобиля может быть установлена только для определенной марки. Создайте объект автомобиля и проверьте работу методов.
+
+class CarEncapsulation {
+	private var brand: String = "TOYOTA"
+	private var model: String = ""
+
+	func setInfo(brand: String, model: String) {
+		if self.brand == brand {
+			self.brand = brand
+			self.model = model
+			return
+		}
+		print("Can set only \(self.brand)")
+	}
+
+	func getInfo() {
+		print("Brand: \(self.brand). \n Model: \(self.model)")
+	}
+}
+
+let carEnc = CarEncapsulation()
+carEnc.setInfo(brand: "KIA", model: "K5")
+carEnc.getInfo()
+carEnc.setInfo(brand: "TOYOTA", model: "K5")
+carEnc.getInfo()
+
