de05a98470d9f47b5ca42bafd167792040deda20
kabulov <n.kabulov@cft.ru>
2024-04-22 21:01:30 +0500

    made logic of tic tac toe

---
+++ helloworld/Model/XO.swift
@@ -0,0 +1,13 @@
+//
+//  XO.swift
+//  helloworld
+//
+//  Created by Nursultan Kabulov on 22.04.2024.
+//
+
+import Foundation
+
+
+struct XO {
+	var label: String?
+}
--- helloworld/helloworld/ViewController.swift
+++ helloworld/helloworld/ViewController.swift
@@ -9,39 +9,57 @@ import UIKit
 
 class ViewController: UIViewController {
 
-	private var isPlayer1: Bool = true
+	@IBOutlet weak var status: UILabel!
 
 	@IBOutlet var xoButtons: [UIButton]!
 
 	@IBOutlet weak var restartButton: UIButton!
 
+	private var controller = TicTacToeController()
+
 	override func viewDidLoad() {
 		super.viewDidLoad()
+		self.updateView()
 		// Do any additional setup after loading the view.
 	}
 
 	@IBAction func clickButton(_ sender: UIButton) {
-		if isPlayer1 {
-			isPlayer1 = false
-			sender.setTitle("❌", for: .normal)
-			sender.isEnabled = false
-		} else {
-			isPlayer1 = true
-			sender.setTitle("⭕️", for: .normal)
-			sender.isEnabled = false
-		}
+		guard let indexOfButton = xoButtons.firstIndex(of: sender) else { return }
+		controller.choiceXO(for: indexOfButton)
+		self.updateView()
 	}
 
+	private func updateView() {
+		for i in xoButtons.indices {
+			let button = xoButtons[i]
+			let XO = controller.arrayXO[i]
+			if let label = XO.label {
+				button.setTitle(label, for: .normal)
+				button.isEnabled = false
+			} else {
+				button.setTitle("", for: .normal)
+				button.isEnabled = true
+			}
+		}
+
+		if let win = controller.findWinner() {
+			self.status.text = "Winner \(win)"
+			xoButtons.forEach { button in
+				button.isEnabled = false
+			}
+		}
+
+		if controller.counter == 0 {
+			self.restartButton.isHidden = true
+		} else {
+			self.restartButton.isHidden = false
+		}
+	}
 
 	@IBAction func restartButton(_ sender: UIButton) {
-		for i in xoButtons {
-			i.setTitle("", for: .normal)
-			i.isEnabled = true
-		}
-	}
-
-	func findWinner() {
-
+		controller = TicTacToeController()
+		self.status.text = "TIC TAC TOE"
+		self.updateView()
 	}
 }
 
