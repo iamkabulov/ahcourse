76ecaa903835258218a08ab8067e8bfb8e22569e
kabulov <n.kabulov@cft.ru>
2024-04-27 21:46:58 +0600

    delete from project main storyboard

---
+++ appNoStoryboard/appNoStoryboard/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  appNoStoryboard
+//
+//  Created by Nursultan Kabulov on 27.04.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		self.view.backgroundColor = .blue
+		// Do any additional setup after loading the view.
+	}
+
+
+}
+
