faac34ca82f71c946614a95d3f09ede71d3b9dd4
kabulov <n.kabulov@cft.ru>
2024-05-30 18:53:55 +0500

    сделал userDefaults для watch list

---
+++ appNoStoryboard/appNoStoryboard/UserDefaults.swift
@@ -0,0 +1,8 @@
+//
+//  UserDefaults.swift
+//  appNoStoryboard
+//
+//  Created by Nursultan Kabulov on 30.05.2024.
+//
+
+import Foundation
---
+++ appNoStoryboard/appNoStoryboard/WatchList/WatchListViewController.swift
@@ -0,0 +1,8 @@
+//
+//  WatchListViewController.swift
+//  appNoStoryboard
+//
+//  Created by Nursultan Kabulov on 30.05.2024.
+//
+
+import Foundation
