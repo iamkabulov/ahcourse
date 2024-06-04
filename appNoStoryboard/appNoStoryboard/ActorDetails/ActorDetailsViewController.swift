f51b654fe04ccff7e6cab2798ff249d0f225cd9d
kabulov <n.kabulov@cft.ru>
2024-06-04 22:26:43 +0500

    Сделал ActorDetailsViewController

---
+++ appNoStoryboard/appNoStoryboard/ActorDetails/ActorDetailsViewController.swift
@@ -0,0 +1,8 @@
+//
+//  ActorDetailViewController.swift
+//  appNoStoryboard
+//
+//  Created by Nursultan Kabulov on 04.06.2024.
+//
+
+import Foundation
---
+++ appNoStoryboard/appNoStoryboard/ActorDetails/ActorEntity.swift
@@ -0,0 +1,8 @@
+//
+//  ActorEntity.swift
+//  appNoStoryboard
+//
+//  Created by Nursultan Kabulov on 04.06.2024.
+//
+
+import Foundation
